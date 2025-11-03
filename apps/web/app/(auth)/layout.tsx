894dc2ec4ff88eecd49659b95ce73f4c66b5c8e6
Hassan <Hassanmaan705@gmail.com>
2025-11-03 15:54:01 +0200

    feat(auth): add common layout for /login and /sign-up

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+// apps/web/app/(auth)/layout.tsx
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex items-center justify-center min-h-dvh">{children}</div>
+  );
+}
